cbb469ef40acb6fac279db073e43b1b05ee4e767
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-08-06 13:34:24 -0300

    criando readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Projeto Telefone Samsung
+
+Criei o layout do aplicativo de telefone da Samsung.
+
+A ideia do projeto surgiu enquanto fazia um curso da Alura.
+
+Com isso, resolvi aplicar meus novos conhecimentos em JS para criar o código.
+
+A primeira versão continha os elementos do HTML criados dentro do mesmo, e o JS apenas puxava os elementos.
+
+Nessa versão final, todo contéudo das teclas do telefone foram criados dentro do JS.
+
+Clique aqui[https://brunosabbagmachado.github.io/telefone-Samsung/] para ver o projeto;
--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ var display = document.querySelector('.display'); //puxa o display do HT
 var buttonValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"];
 var button = [];
 
-function tocaSom (){
-    document.querySelector('#som_tecla').play();
-}
-
 document.addEventListener('DOMContentLoaded', function () {
 
     for (var i = 0; i < buttonValues.length; i++) {
@@ -15,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
         button[i].value = buttonValues[i];
         button[i].onclick = function () {
             display.value += this.value;
-            tocaSom();
+            document.querySelector('#som_tecla').play();
         }
     }
 
